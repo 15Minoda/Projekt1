b445001dce019d7c22cbc8690eaf51477b7661ef
Michael <gmx@michael-drost.de>
2021-06-26 14:46:15 +0200

    new Version 0.001

--- Module1.py
+++ Module1.py
@@ -1,9 +1,9 @@
 class LivingThing():
-    __level   = 0
     __actHP   = 0
     __nextExp = 0
 
     def __init__ (self, name, maxHP):
+        self.__level   = 0
         self.name   = name
         self.exp    = 0
         self.__maxHP  = maxHP
@@ -15,15 +15,18 @@ class LivingThing():
         print("Was Created!")
 
     def Show (self):
-        print("Name: "+self.name+"Level: "+str(self.__level))
+        print("Name: "+self.name+" Level: "+str(self.__level))
         print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
         print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
 
     def __LevelUp(self):
         self.__level += 1
         print("Level Up! ->"+str(self.__level))
-        self.__nextExp *= self.__level * 1.89
+        self.__nextExp *= self.__level * 2.5
         self.__maxHP += 50
+        if self.exp >= self.__nextExp:            
+            print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
+            self.__LevelUp()
 
     def __del__(self):
         print(self.name+" was Removed")
@@ -35,26 +38,55 @@ class LivingThing():
         if self.exp >= self.__nextExp:
             self.__LevelUp()
 
+    #def ReturnExpOnDeath(self):
+    #    return self.__level * 10
+
+    def Death(self):
+            print(self.name+" is Dead")
+            self.__del__()
+
     def SubHP(self, val):
         self.__actHP -= val
         print("HP decreased: "+str(val))
         print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
         if self.__actHP <= 0:
-            print(self.name+" is Dead")
-            self.__del__()
+            #self.ReturnExpOnDeath()
+            self.Death()
     
 
 class Human(LivingThing):
-    __level   = 0
-    __actHP   = 0
-    __nextExp = 0
+    #__level   = 0
+    #__maxHP = 0
+    #__nextExp = 0
+    __skillPoints = 0
 
     def __init__(self, name, maxHP):
+        self.__nextExp = 100
+        self.__level = 0
+        self.__maxHP = maxHP
+        self.__actHP = maxHP
         super().__init__(name, maxHP)       
 
     def __LevelUp(self):
         self.__level += 1
+        self.__skillPoints += 5
         print("Level Up! ->"+str(self.__level))
+        print("Available Skillpoints:"+str(self.__skillPoints))
         self.__nextExp *= self.__level * 2.5
         self.__maxHP *= 1.2
-
+        if self.exp >= self.__nextExp:            
+            print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
+            self.__LevelUp()
+        
+    def AddExp(self, exp):
+        self.exp += exp
+        print("+"+str(exp)+"EXP")
+        print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
+        if self.exp >= self.__nextExp:
+            self.__LevelUp()
+   
+    def Show (self):
+        print("Name: "+self.name+" Level: "+str(self.__level))
+        print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
+        print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
+        print("Available Skillpoints:"+str(self.__skillPoints))
--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import Module1 
+import ObserverPattern_Test#
+import Decorator
+
+from datetime import datetime
+
+now = datetime.now()
 
 def main():
-    print("Hello")
+    print("Hello new Start "+ str(now))
 
-    Mino = Module1.Human("Minoda", 20)
-    Test = Module1.LivingThing("testvieh", 50)
+    Mino = Module1.Human("Minoda", 50)
+    #Test = Module1.LivingThing("testvieh", 50)
 
-    Test.AddExp(200)
+    #Test.AddExp(200)
+    print("action1")
     Mino.AddExp(20)
+    print("action2")
     Mino.AddExp(80)
+    print("action3")
     Mino.SubHP(18)
-    Mino.AddExp(90)
+    print("action4")
+    Mino.AddExp(5000)
+    print("action5")
     Mino.SubHP(3)
-    Test.SubHP(30)
-    Mino.Show
+    print("action6")
+    #Test.SubHP(30)
+    Mino.Show()
 
 
 
