b690afbbd6f2d5bcc22a903e1467e79515acbffb
Michael <gmx@michael-drost.de>
2021-06-26 15:31:31 +0200

    Version 0.0.0.1

--- Module1.py
+++ Module1.py
@@ -1,25 +1,38 @@
 class LivingThing():
     __actHP   = 0
     __nextExp = 0
+    actionCounter = 0
 
-    def __init__ (self, name, maxHP):
+    def __init__ (self, name, maxHP, debug = 0):
+        self.debug = debug
         self.__level   = 0
         self.name   = name
         self.exp    = 0
         self.__maxHP  = maxHP
         self.__actHP  = maxHP    
         self.__nextExp = 100;  
+        print("##############################################")
         print("Name: "+self.name+" Level: "+str(self.__level))
         print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
         print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
         print("Was Created!")
+        print("##############################################")
+
+    def NewAction(self):
+        if self.debug > 0:
+            LivingThing.actionCounter+=1
+            print("--------------------Action_"+str(LivingThing.actionCounter))
 
     def Show (self):
+        self.NewAction()
+        print("##############################################")
         print("Name: "+self.name+" Level: "+str(self.__level))
         print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
         print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
+        print("##############################################")
 
     def __LevelUp(self):
+        self.NewAction()
         self.__level += 1
         print("Level Up! ->"+str(self.__level))
         self.__nextExp *= self.__level * 2.5
@@ -29,9 +42,11 @@ class LivingThing():
             self.__LevelUp()
 
     def __del__(self):
+        self.NewAction()
         print(self.name+" was Removed")
 
     def AddExp(self, exp):
+        self.NewAction()
         self.exp += exp
         print("+"+str(exp)+"EXP")
         print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
@@ -42,10 +57,12 @@ class LivingThing():
     #    return self.__level * 10
 
     def Death(self):
-            print(self.name+" is Dead")
-            self.__del__()
+        self.NewAction()
+        print(self.name+" is Dead")
+        self.__del__()
 
     def SubHP(self, val):
+        self.NewAction()
         self.__actHP -= val
         print("HP decreased: "+str(val))
         print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
@@ -60,25 +77,28 @@ class Human(LivingThing):
     #__nextExp = 0
     __skillPoints = 0
 
-    def __init__(self, name, maxHP):
+    def __init__(self, name, maxHP, debug):      
+        self.debug = debug
         self.__nextExp = 100
         self.__level = 0
         self.__maxHP = maxHP
         self.__actHP = maxHP
-        super().__init__(name, maxHP)       
+        super().__init__(name, maxHP, debug)       
 
     def __LevelUp(self):
+        super().NewAction()
         self.__level += 1
         self.__skillPoints += 5
         print("Level Up! ->"+str(self.__level))
         print("Available Skillpoints:"+str(self.__skillPoints))
         self.__nextExp *= self.__level * 2.5
-        self.__maxHP *= 1.2
+        self.__maxHP *= 1.5
         if self.exp >= self.__nextExp:            
             print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
             self.__LevelUp()
         
     def AddExp(self, exp):
+        super().NewAction()
         self.exp += exp
         print("+"+str(exp)+"EXP")
         print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
@@ -86,7 +106,14 @@ class Human(LivingThing):
             self.__LevelUp()
    
     def Show (self):
+        super().NewAction()
+        print("##############################################")
         print("Name: "+self.name+" Level: "+str(self.__level))
         print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
         print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
         print("Available Skillpoints:"+str(self.__skillPoints))
+        print("##############################################")
+
+class Creature(LivingThing):
+    def __init__(self, name, maxHP, debug):
+        super().__init__(name, maxHP, debug=debug)
--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import Module1 
-import ObserverPattern_Test#
+import ObserverPattern_Test
 import Decorator
 
 from datetime import datetime
 
 now = datetime.now()
+DEBUG = 1
 
 def main():
     print("Hello new Start "+ str(now))
 
-    Mino = Module1.Human("Minoda", 50)
-    #Test = Module1.LivingThing("testvieh", 50)
+    Mino = Module1.Human("Minoda", 50, DEBUG)
+    Test = Module1.Creature("testvieh", 50, DEBUG)
 
-    #Test.AddExp(200)
-    print("action1")
+    Test.AddExp(200)
     Mino.AddExp(20)
-    print("action2")
     Mino.AddExp(80)
-    print("action3")
     Mino.SubHP(18)
-    print("action4")
     Mino.AddExp(5000)
-    print("action5")
-    Mino.SubHP(3)
-    print("action6")
-    #Test.SubHP(30)
+    #Test.AddExp(5000)
+    Mino.SubHP(500)
+    #Test.Show()
     Mino.Show()
 
 
