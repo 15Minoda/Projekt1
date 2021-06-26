d57f22c830865d442192b7fe72aeb12b97728543
Michael <gmx@michael-drost.de>
2021-06-26 14:02:43 +0200

    firstTests

--- Module1.py
+++ Module1.py
@@ -26,18 +26,21 @@ class LivingThing():
         self.__maxHP += 50
 
     def __del__(self):
-        print(self.name+" is Dead")
+        print(self.name+" was Removed")
 
     def AddExp(self, exp):
         self.exp += exp
-        print("+"+str(self.exp)+"EXP")
+        print("+"+str(exp)+"EXP")
+        print("Exp:"+str(self.exp)+"/"+str(self.__nextExp))
         if self.exp >= self.__nextExp:
             self.__LevelUp()
 
     def SubHP(self, val):
         self.__actHP -= val
+        print("HP decreased: "+str(val))
         print("HP:"+str(self.__actHP)+"/"+str(self.__maxHP))
         if self.__actHP <= 0:
+            print(self.name+" is Dead")
             self.__del__()
     
 
@@ -48,20 +51,10 @@ class Human(LivingThing):
 
     def __init__(self, name, maxHP):
         super().__init__(name, maxHP)       
-        self.name   = name
-        self.exp    = 0
-        self.__maxHP  = maxHP
-        self.__actHP  = maxHP    
-        self.__nextExp = 100;  
 
     def __LevelUp(self):
         self.__level += 1
         print("Level Up! ->"+str(self.__level))
-        self.__nextExp *= self.__level * 1.89
+        self.__nextExp *= self.__level * 2.5
         self.__maxHP *= 1.2
 
-    def AddExp(self, exp):
-        self.exp += exp
-        print("+"+str(self.exp)+"EXP")
-        if self.exp >= self.__nextExp:
-            self.__LevelUp()
--- main.py
+++ main.py
@@ -4,12 +4,15 @@ def main():
     print("Hello")
 
     Mino = Module1.Human("Minoda", 20)
+    Test = Module1.LivingThing("testvieh", 50)
 
+    Test.AddExp(200)
     Mino.AddExp(20)
     Mino.AddExp(80)
     Mino.SubHP(18)
-    
+    Mino.AddExp(90)
     Mino.SubHP(3)
+    Test.SubHP(30)
     Mino.Show
 
 
